adaf261c8c077f8ea9f4062915eda368d2fcbb8b
kohakuma <kohakuma4m@gmail.com>
2017-04-21 11:46:02 -0400

    Undone that space !

--- ProjetRDV247/Service1.svc.cs
+++ ProjetRDV247/Service1.svc.cs
@@ -15,7 +15,6 @@ namespace ProjetRDV247
         public string GetData(int value)
         {
             return string.Format("You entered: {0}", value);
-
         }
 
         public CompositeType GetDataUsingDataContract(CompositeType composite)

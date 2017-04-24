f047ef20fb52053a4494ec59fe0929b3dfab7e3b
kohakuma <kohakuma4m@gmail.com>
2017-04-24 16:25:06 -0400

    ajout package controle et service wcf

---
+++ ProjetRDV247/Controle/IServiceRDV247.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ProjetRDV247.Controle
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom d'interface "IServiceRDV247" à la fois dans le code et le fichier de configuration.
+    [ServiceContract]
+    public interface IServiceRDV247
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "json/{id}")]
+        void DoWork();
+    }
+}
---
+++ ProjetRDV247/Controle/ServiceRDV247.svc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ProjetRDV247.Controle
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "ServiceRDV247" à la fois dans le code, le fichier svc et le fichier de configuration.
+    // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez ServiceRDV247.svc ou ServiceRDV247.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
+    public class ServiceRDV247 : IServiceRDV247
+    {
+        #region IRestService Members
+
+        public void DoWork()
+        {
+        }
+
+        #endregion
+    }
+}
